215862d3ea7ecdfb2401ff4e27e1bbc745f7d918
DESKTOP-MOTPEKH\REAL <real_striver@outlook.com>
2022-05-25 21:45:32 +0900

    update readme

--- contract/README.md
+++ contract/README.md
@@ -1,5 +1,14 @@
 # MyNFT Hardhat Project
 
+```
+Should create config\config.json
+{
+    "api_key": "your_etherscan_api_key",
+    "p_key": "private_key_for_minter_wallet"
+}
+
+For the minter wallet, please check the api\config\config.json
+```
 
 ```shell
 npx hardhat accounts
--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -3,13 +3,14 @@ const { ethers } = require("hardhat");
 async function main() {
     const [deployer] = await ethers.getSigners();
 
-    // const myNFT = await ethers.getContractAt("MyNFT", "");
+    const myNFT = await ethers.getContractAt("MyNFT", "0xFb470DFbd969Ed0858a61EC5d7Fb8CB96ea12692");
+    await myNFT.setMinter("0xFC38390A8621c76c4e5e7b5451a844995086dAfa");
     
     // MyNFT contract
-    const MyNFT = await ethers.getContractFactory("MyNFT");
-    const myNFT = await MyNFT.deploy();
-    await myNFT.deployed();
-    console.log(`mynft contract deployed to: ${myNFT.address}`);
+    // const MyNFT = await ethers.getContractFactory("MyNFT");
+    // const myNFT = await MyNFT.deploy();
+    // await myNFT.deployed();
+    // console.log(`mynft contract deployed to: ${myNFT.address}`);
 }
 
 main()
